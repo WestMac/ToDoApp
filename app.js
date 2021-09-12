aa63859ca1df45a0a358b5e02b40a5583580660a
WestMac <wester.maciej@gmail.com>
2021-09-12 21:15:44 +0200

    test

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
+
 app.use(cookieParser())
 
 app.use('/', userRoutes)

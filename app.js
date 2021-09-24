4d3ad2a57afedd01d5e62ddfce7026c42b78007a
WestMac <wester.maciej@gmail.com>
2021-09-24 11:50:51 +0200

    Resolving problem with tests passing locally but not on CI

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
 const app = express();
-const userRoutes = require('./routes/users')
-const listRoutes = require('./routes/list')
-const { sequelize } = require('./models')
-const methodOverride = require('method-override');
+const userRoutes = require("./routes/users");
+const listRoutes = require("./routes/list");
+const { sequelize } = require("./models");
+const methodOverride = require("method-override");
+const bcrypt = require("bcrypt");
 
-
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname,'public')));
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-    next();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Authorization");
+  next();
 });
-app.use(cookieParser())
+app.use(cookieParser());
 
 
-
-app.use('/', userRoutes)
-app.use('/list', listRoutes)
+app.use("/", userRoutes);
+app.use("/list", listRoutes);
 sequelize.sync();
 
-
-
-module.exports = app
+module.exports = app;
--- seeders/20210923091210-demo_user.js
+++ seeders/20210923091210-demo_user.js
@@ -3,13 +3,12 @@ const bcrypt = require('bcrypt')
 const { User, refreshToken, Sequelize } = require("../models");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // let test = await bcrypt.hash('test', 10)
-    // let editor = await bcrypt.hash('editor', 10)
-
+    
+   
     return queryInterface.bulkInsert('Users', 
     [
-      { username: 'test', password: hashPassword('test'), email: 'test@test.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'editor', password: hashPassword('editor') , email: 'editor@editor.com', createdAt: new Date(), updatedAt: new Date() }
+      { username: 'test', password: bcrypt.hashSync('test', Number(process.env.SALT)), email: 'test@test.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'editor', password: bcrypt.hashSync('editor', Number(process.env.SALT)) , email: 'editor@editor.com', createdAt: new Date(), updatedAt: new Date() }
     ]
       );
   },
@@ -20,9 +19,3 @@ module.exports = {
 };
 
 
-function hashPassword(password) {
-  let hash = bcrypt.hash(password, Number(process.env.SALT), async function (err, hash) {
-    return hash
-  })
-  return hash
-}

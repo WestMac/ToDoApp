ff7514a8b837a1c1ccede164cc9bbc929e23870f
WestMac <wester.maciej@gmail.com>
2021-09-24 11:21:10 +0200

    Resolving problem with tests passing locally but not on CI

--- seeders/20210923091210-demo_user.js
+++ seeders/20210923091210-demo_user.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcrypt')
 const { User, refreshToken, Sequelize } = require("../models");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    let test = await bcrypt.hash('test', 10)
-    let editor = await bcrypt.hash('editor', 10)
+    // let test = await bcrypt.hash('test', 10)
+    // let editor = await bcrypt.hash('editor', 10)
 
     return queryInterface.bulkInsert('Users', 
     [
-      { username: 'test', password: test, email: 'test@test.com', createdAt: new Date(), updatedAt: new Date() },
-      { username: 'editor', password: editor , email: 'editor@editor.com', createdAt: new Date(), updatedAt: new Date() }
+      { username: 'test', password: hashPassword('test'), email: 'test@test.com', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'editor', password: hashPassword('editor') , email: 'editor@editor.com', createdAt: new Date(), updatedAt: new Date() }
     ]
       );
   },
@@ -18,3 +18,11 @@ module.exports = {
     return queryInterface.bulkDelete('Users', null, {});
   }
 };
+
+
+function hashPassword(password) {
+  bcrypt.hash(password, Number(process.env.SALT), async function (err, hash) {
+    return hash
+  })
+  return hash
+}

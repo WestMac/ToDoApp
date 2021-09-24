30e365cc29d34fcee9ad99f5c330c676e288b3ef
WestMac <wester.maciej@gmail.com>
2021-09-24 12:20:40 +0200

    Resolving problem with tests passing locally but not on CI

--- tests/user.test.js
+++ tests/user.test.js
@@ -10,15 +10,6 @@ describe("POST login/register", () => {
     
     describe("given a username and a password", () => {
         
-        test("Should respond with 302", async () => {
-            const response = await request(app).post("/register").send({
-                username: 'newUser',
-                password: 'newUserrsrs',
-                email: 'newUser@newUser.pl'
-            })
-            expect(response.statusCode).toBe(302)
-       
-        })
         test('Should respond with 302', async() => {
             const response = await request(app).post('/login').send({
                 username: 'test',
@@ -28,6 +19,17 @@ describe("POST login/register", () => {
             expect(response.statusCode).toBe(302)
             
         })
+        
+        test("Should respond with 302", async () => {
+            const response = await request(app).post("/register").send({
+                username: 'newUser',
+                password: 'newUserrsrs',
+                email: 'newUser@newUser.pl'
+            })
+            expect(response.statusCode).toBe(302)
+       
+        })
+       
         test("Should respond with 403", async () => {
             const response = await request(app).post("/register").send({
                 username: null,
@@ -58,28 +60,31 @@ describe("POST login/register", () => {
         test("Should respond with 403",async () => {
             const response = await request(app).post('/login').send({
                 username: 'test',
-                password: 'test',
+                password: null,
             })
             expect(response.statusCode).toBe(400)
             
         })
         test("Should respond with 403",async () => {
             const response = await request(app).post('/login').send({
-                username: 'test',
-                password: 'test',
+                username: undefined,
+                password: null,
             })
             expect(response.statusCode).toBe(400)
          
         })
         test("Should respond with 403",async () => {
             const response = await request(app).post('/login').send({
-                username: 'test',
-                password: 'test',
+                username: 'elo',
+                password: 'testtesttest',
             })
             expect(response.statusCode).toBe(400)
         })
     })
 
+    describe("when the username or password is missing", () => {
+
+    })
 
     afterAll(async () => {
         await sequelize.close()
